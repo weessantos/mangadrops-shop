55f4e70c70d04680989a54a054e2a3ed2a21d0ae
Wesley Santos <39712985+weessantos@users.noreply.github.com>
2026-02-08 13:28:13 -0300

    create js file

---
+++ js/script.js
@@ -0,0 +1,26 @@
+    function toggleCategory(card) {
+      const sub = card.nextElementSibling;
+      sub.classList.toggle('open');
+
+        const cta = card.querySelector('.cta');
+        cta.innerText = sub.classList.contains('open')
+    ? 'Ocultar volumes ⌃'
+    : 'Ver volumes ⌄';
+    }
+
+    // Microlink — apenas para IMAGEM
+    document.querySelectorAll('.card[data-url]').forEach(card => {
+      const url = card.getAttribute('data-url');
+
+      fetch(`https://api.microlink.io/?url=${encodeURIComponent(url)}`)
+        .then(res => res.json())
+        .then(data => {
+          if (!data || !data.data) return;
+
+          const img = data.data.image?.url;
+          if (img) {
+            card.querySelector('.thumb').src = img;
+          }
+        })
+        .catch(() => {});
+    });
